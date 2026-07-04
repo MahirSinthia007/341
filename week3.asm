13a068f229e1a28f4ef7e7750c1a74f09e31e1b1
Mahir Hasan <mahir.abrar.hasan@g.bracu.ac.bd>
2026-07-04 12:54:51 +0600

    Update week3.asm explicit loop

--- week3.asm
+++ week3.asm
@@ -158,3 +158,53 @@ INT 21H
 
 MAIN ENDP
     END MAIN
+
+
+.MODEL SMALL
+ 
+.STACK 100H
+
+.DATA
+
+; declare variables here
+
+.CODE
+MAIN PROC
+
+; initialize DS
+
+MOV AX,@DATA
+MOV DS,AX
+ 
+; enter your code here
+
+MOV BL,61h
+MOV AH,2
+
+LOOP_START:          ;explicit loop
+
+     CMP BL,66h
+     JG END_BLOCK
+     MOV ah,2
+     MOV dl,bl
+     INT 21h
+     INC BL
+     MOV AH,2
+     MOV DL, 10
+     INT 21h 
+     MOV AH,2
+     MOV DL,13
+     INT 21h
+     JMP LOOP_START
+
+END_BLOCK:
+
+ 
+
+;exit to DOS
+               
+MOV AX,4C00H
+INT 21H
+
+MAIN ENDP
+    END MAIN
