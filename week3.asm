118a8570b46619b2702bfee87c7862741d8df847
Mahir Hasan <mahir.abrar.hasan@g.bracu.ac.bd>
2026-07-11 11:19:17 +0600

    Implement character input and display conversion
    Added code to read a character input, convert it, and display it.

--- week3.asm
+++ week3.asm
@@ -208,3 +208,57 @@ INT 21H
 
 MAIN ENDP
     END MAIN
+
+
+.MODEL SMALL
+ 
+.STACK 100H
+
+.DATA
+
+; declare variables here
+
+.CODE
+MAIN PROC
+
+; initialize DS
+
+MOV AX,@DATA
+MOV DS,AX
+ 
+; enter your code here
+
+mov ah,1
+int 21h
+
+mov cl,al
+
+sub cl,20h
+
+
+mov ah,2
+mov dl,10
+int 21h
+
+mov ah,2
+mov dl,13
+int 21h
+
+mov ah,2
+mov dl,cl
+int 21h
+
+
+
+
+
+
+ 
+
+;exit to DOS
+               
+MOV AX,4C00H
+INT 21H
+
+MAIN ENDP
+    END MAIN
