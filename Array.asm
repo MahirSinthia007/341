ecf3b90f05a89bd02bfe5d95455f1fed58f5dcd8
Mahir Hasan <mahir.abrar.hasan@g.bracu.ac.bd>
2026-08-11 02:01:05 +0600

    Implement array display in assembly
    Added assembly code to display an array of numbers.

--- Array.asm
+++ Array.asm
@@ -46,6 +46,62 @@ end:
 ;exit to DOS
                
 MOV AX,4C00H
+
+
+
+
+
+.MODEL SMALL
+ 
+.STACK 100H
+
+.DATA
+
+; declare variables here
+
+a db 1,2,3,4,5
+
+.CODE
+MAIN PROC
+
+; initialize DS 
+
+
+
+
+MOV AX,@DATA
+MOV DS,AX
+ 
+; enter your code here
+
+ 
+mov cx,5
+mov ah,2
+mov si,0
+start:
+mov dl,a[si]
+add dl,30H
+int 21h
+inc si 
+loop start
+
+
+
+
+
+end:
+
+
+ 
+
+;exit to DOS
+               
+MOV AX,4C00H
+INT 21H
+
+MAIN ENDP
+    END MAIN
+
 INT 21H
 
 MAIN ENDP
