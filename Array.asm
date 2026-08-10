ff2fa7abd07dc7445b4e6d448067086b13d78dbd
Mahir Hasan <mahir.abrar.hasan@g.bracu.ac.bd>
2026-08-11 01:57:02 +0600

    Add assembly program to display array elements

---
+++ Array.asm
@@ -0,0 +1,52 @@
+.MODEL SMALL
+ 
+.STACK 100H
+
+.DATA
+
+; declare variables here
+
+a db 1,2,3,4,5
+
+.CODE
+MAIN PROC
+
+; initialize DS 
+
+
+
+
+MOV AX,@DATA
+MOV DS,AX
+ 
+; enter your code here
+
+mov cx,5
+mov ah,2
+lea si,a
+start:
+ mov dl,[si]
+ add dl,30H
+ int 21h
+ inc si
+ loop start
+  
+ 
+
+
+
+
+
+
+end:
+
+
+ 
+
+;exit to DOS
+               
+MOV AX,4C00H
+INT 21H
+
+MAIN ENDP
+    END MAIN
