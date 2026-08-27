c30d7877702d99522e099ea6f21bb05b3b1cfb5c
Mahir Hasan <mahir.abrar.hasan@g.bracu.ac.bd>
2026-08-28 01:47:29 +0600

    Add macro to reverse a string in assembly

---
+++ Reverse_Macro.asm
@@ -0,0 +1,84 @@
+.MODEL SMALL
+
+Reverse_String Macro string,length,result
+    mov di,0
+    mov si,0
+    Main_push_loop:
+     cmp di,len
+     JE Main_pop_loop
+     mov al,string[di]
+     mov ah,0
+     push ax
+     add di,1
+     jmp Main_push_loop
+     
+    Main_pop_loop:
+     cmp si,len
+     JE exit_mac
+     pop ax
+     mov result[si],al
+     add si,1
+     jmp Main_pop_loop
+    exit_mac:
+    
+     
+     
+
+endm
+     
+
+
+
+
+
+
+
+ 
+.STACK 100H
+
+.DATA
+
+; declare variables here
+string db "Mahir$"
+len  dw 5
+result db 5 dup(?)
+
+
+
+.CODE
+MAIN PROC
+
+; initialize DS
+
+MOV AX,@DATA
+MOV DS,AX
+ 
+; enter your code here 
+
+
+Reverse_String string,len,result
+
+
+
+ 
+
+;exit to DOS
+               
+MOV AX,4C00H
+INT 21H
+
+MAIN ENDP
+
+   Display_string PROC
+    MOV ah,9
+    int 21h
+    ret
+    
+   display_string endp
+    
+    
+    
+    
+    
+    
+    END MAIN
